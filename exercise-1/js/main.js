789525ec8aed279c48e1a85554d8cae7c71aaf48
Nico Malig <nicomalig@gmail.com>
2016-10-13 10:19:59 -0700

    partial e1 completion

--- exercise-1/js/main.js
+++ exercise-1/js/main.js
@@ -3,16 +3,16 @@
 // You'll have to wait for you page to load to assign events to the elements created in your index.html file
 $(function() {
     // Use $.get to read in your `data/students.csv` dataset: remember, you must be running a local server
-
-        // Parse the data using Papa.parse
-
-
+    $.get('data/students.csv', function(data, error) {
+      // Parse the data using Papa.parse
+      var parsedData = Papa.parse(data, {
+        header: true
+      }).data;
         // Use jQuery to create a table, and store that in a variable
-
+        var table = $('<table>');
 
         // Append a table header for each key in your first observation
-
-
+        
         // Iterate through your array and create a new table row for each element in your array
 
 
